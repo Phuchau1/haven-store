6925075cfe81ee3e9cce1c06c38a7e31e0578b22
Phuchau1 <ntphau21@gmail.com>
2026-08-21 11:13:49 +0700

    chore: clean up all redundant scratch files, logs, and update .gitignore

--- backend/scratch_cats.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI)
-.then(async () => {
-    const categories = await mongoose.connection.db.collection('categories').find({}).toArray();
-    console.log(JSON.stringify(categories, null, 2));
-    process.exit(0);
-});
